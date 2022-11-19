8e90e0b4eefc42934c81c75dc4a08ce274154b99
Duru C. Mikael <durumikaelc@gmail.com>
2022-11-19 21:34:55 +0100

    reduce user name font size

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class RandomUsers extends Component {
           <main>
             <section className="container w-100 min-vh-100 d-flex justify-content-center align-items-center">
               {users.map((user) => (
-                <div className="card text-white bg-primary my-5  py-4 sm:w-50" style={{maxWidth: "600px"}} key='1' >
+                <div className="card text-white bg-primary my-5  py-4 sm:w-50" style={{maxWidth: "450px"}} key='1' >
                   <div className="img-container w-50 mx-auto mt-2">
                     <Image picture={user.picture} name={user.name} />
                   </div>
@@ -64,7 +64,7 @@ class RandomUsers extends Component {
 
 const Image = ({ picture, name }) => <img src={picture.medium} className="card-img-top img-fluid rounded-circle border border-5 border-white mx-auto mb-4" alt={name.first} id='image' />;
 
-const UserName = ({ name }) => <h2 className="display-4 lead fw-bold pb-3 text-center">{name.title} {name.first} {name.last}</h2>;
+const UserName = ({ name }) => <h2 className="display-6 lead fw-bold pb-3 text-center">{name.title} {name.first} {name.last}</h2>;
 
 const UserID = ({ login }) => <p className='fs-5 fw-light d-flex align-items-center gap-3 py-1'><i className="fa fa-user" /> <span>{login.username}</span></p>;
 
